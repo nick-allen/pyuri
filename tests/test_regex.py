bc5f03b9cdc78129b496a770c802afe5e4b0443c
Nick Allen <nick.allen.cse@gmail.com>
2016-07-30 22:46:30 -0400

    Get URI passing some initial tests

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -1,32 +1,62 @@
-from nose.tools import assert_equals, assert_is_not_none
+from nose.tools import assert_dict_equal, assert_is_not_none
 
 from uri.uri import URI_REGEX
 
 
-valid_uris = [
+valid_uris = (
 	('https://google.com/', {
 		'scheme': 'https',
-		'user': None,
-		'pass': None,
+		'username': None,
+		'password': None,
 		'host': 'google.com',
 		'port': None,
 		'path': '/',
 		'query': None,
 		'fragment': None
+	}),
+	('postgres://user:pass@127.0.0.1:5432/db', {
+		'scheme': 'postgres',
+		'username': 'user',
+		'password': 'pass',
+		'host': '127.0.0.1',
+		'port': '5432',
+		'path': '/db',
+		'query': None,
+		'fragment': None
+	}),
+	('ftp://user@ftp.com?query=value', {
+		'scheme': 'ftp',
+		'username': 'user',
+		'password': None,
+		'host': 'ftp.com',
+		'path': None,
+		'port': None,
+		'query': 'query=value',
+		'fragment': None
+	}),
+	('localhost:8080/', {
+		'scheme': None,
+		'username': None,
+		'password': None,
+		'host': 'localhost',
+		'port': '8080',
+		'query': None,
+		'fragment': None,
+		'path': '/'
 	})
-]
+)
 
 
-def verify_regex_matches(uri_string, match_dict):
+def verify_regex_matches(uri_string, expected_match_dict):
 	match = URI_REGEX.match(uri_string)
 	assert_is_not_none(match)
 
-	assert_equals(
+	assert_dict_equal(
 		match.groupdict(),
-		match_dict
+		expected_match_dict
 	)
 
 
 def test_uri_regex_matches():
-	for uri_string, match_dict in valid_uris:
-		yield verify_regex_matches, uri_string, match_dict
+	for uri_string, expected_match_dict in valid_uris:
+		yield verify_regex_matches, uri_string, expected_match_dict
--- uri/uri.py
+++ uri/uri.py
@@ -1,7 +1,120 @@
 import os
 import re
 
-with open(os.path.join(os.path.dirname(__file__), 'uri.regexp')) as f:
+from .validators import *
+
+with open(os.path.join(os.path.dirname(__file__), 'uri.regex')) as f:
 	pattern = f.read()
 
 URI_REGEX = re.compile(pattern, flags=re.VERBOSE)
+
+
+class URI(object):
+	"""Data structure abstracting out work needed to parse and build URIs"""
+
+	__modified = False
+	__uri = None
+	__validators = {}
+
+	def __init__(
+		self,
+		uri=None,
+		scheme=None,
+		username=None,
+		password=None,
+		host=None,
+		port=None,
+		path=None,
+		query=None,
+		fragment=None,
+		strict=False
+	):
+		if uri and any([locals()[key] is not None for key in self.__validators]):
+			raise ValueError('Cannot provide uri argument alongside any individual parameters')
+
+		self._strict = strict
+		self.__validators = {
+			'scheme': [StringValidator(self._strict)],
+			'username': [StringValidator()],
+			'password': [StringValidator()],
+			'host': [StringValidator(self._strict)],
+			'port': [PortValidator()],
+			'path': [StringValidator()],
+			'query': [StringValidator()],
+			'fragment': [StringValidator()]
+		}
+
+		if uri is None:
+			self.scheme = scheme
+			self.username = username
+			self.password = password
+			self.host = host
+			self.port = port
+			self.path = path
+			self.query = query
+			self.fragment = fragment
+
+		else:
+			self.uri = uri
+
+	def __str__(self):
+		return self.uri
+
+	def __setattr__(self, key, value):
+		if key in self.__validators:
+			self.__modified = True
+
+			for validator in self.__validators[key]:
+				try:
+					value = validator(value)
+				except ExitValidation:
+					break
+				except Exception as error:
+					raise ValueError('Unable to set `{}` = `{}`: {}'.format(key, value, error))
+
+		return super(URI, self).__setattr__(key, value)
+
+	@property
+	def uri(self):
+		"""Cache to prevent recalculating URI unless necessary"""
+		if self.__modified:
+			self.__uri = self.__parse_uri()
+
+		return self.__uri
+
+	@uri.setter
+	def uri(self, value):
+		"""Attempt to validate URI and split into individual values"""
+		if value == self.__uri:
+			return
+
+		match = URI_REGEX.match(value)
+		if match is None:
+			raise ValueError('Unable to parse URI from `{}`'.format(value))
+
+		for key, value in match.groupdict().items():
+			setattr(self, key, value)
+
+	def __parse_uri(self):
+		"""Parse complete URI from all values"""
+		if self.scheme:
+			scheme = '{}://'.format(self.scheme)
+		else:
+			scheme = ''
+
+		credentials = self.username or ''
+		password = self.password or ''
+
+		if credentials and password:
+			credentials = '{}:{}'.format(credentials, password)
+
+		if self.port:
+			location = '{}:{}'.format(self.host, self.port)
+		else:
+			location = self.host
+
+		path = self.path or ''
+		query = self.query or ''
+		fragment = self.fragment or ''
+
+		return scheme + credentials + location + path + query + fragment
